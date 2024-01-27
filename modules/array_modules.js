55232a54374ed41de257844809fcea30426710c1
eltoski3030 <139253578+eltoski3030@users.noreply.github.com>
2024-01-27 19:42:56 -0400

    Add files via upload

---
+++ modules/array_modules.js
@@ -0,0 +1,12 @@
+// This are module functions to simplify using them. For strings and Arrays.
+// Built in functions might not be added. 
+
+
+
+
+
+export default {
+    function1,
+    function2,
+    variable1,
+  };
